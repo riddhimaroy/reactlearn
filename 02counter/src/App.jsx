1a425acf3c51e409f128ba0d7d9eb0ad9691b8ed
unknown <riddhimaroy10@gmail.com>
2025-04-21 12:45:23 +0530

    fifth video

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(1)
+  
+  // let counter = 5
+
+  const addValue = () => {
+    console.log(counter);
+    counter = counter + 1;
+    setCounter(counter);
+  }
+
+  const subValue = () => {
+    console.log(counter);
+    if (counter>0){
+      counter = counter - 1;
+    }
+    else
+    {
+      alert("too small");
+    }
+    setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Increase</button>
+      <br  />
+      <br  />
+      <button onClick = {subValue}>Decrease</button>
+    </>
+  )
+}
+
+export default App
